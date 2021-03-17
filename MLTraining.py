e377e9df569d0f7de634bd2de1bdba342e864e1e
BowenTwan <bowenduan4344@outlook.com>
2021-03-17 13:54:07 -0400

    machine learning

--- MLTraining.py
+++ MLTraining.py
@@ -7,66 +7,93 @@ from joblib import dump, load
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 
-#* Load Data
-filename = 'A'
+
+#* Getting stock list 
+# getting currecnt working dirctory
 cwd = os.getcwd()
-filepath = f'{cwd}/LIData/li_{filename}.csv'
-df = pd.read_csv(f'{filepath}',index_col='Date')
-# df.set_index(['Date'])
-print(f'{filename} stock training data loaded.')
-print(df.head())
+path = cwd + '/LIData'
 
-#*Prapre Training Data
-# training period: 20110208~20180208
-df_train = df[df.index < '2018-02-08' ]
-# resample the training data 
-sgn_1 = df_train[df_train['sgn'] == 1]
-sgn_0 = df_train[df_train['sgn'] == 0]
-len_1 = len(sgn_1)
-len_0 = len(sgn_0)
-print(f'Up record has {len_1} row, while the down records have {len_0} row \n')
+stocklist = []
+for filename in os.listdir(path):
+    if filename.endswith('.csv'):
+        stocklist.append(filename[3:-4])
+print(f'There are {len(stocklist)} stocks in S&P500 \n')
 
-#resample
-df_majority = sgn_1
-df_minority = sgn_0
+i = 0 
+#* Load Data
+for filename in stocklist:
+    try: 
+        cwd = os.getcwd()
+        filepath = f'{cwd}/LIData/li_{filename}.csv'
+        df = pd.read_csv(f'{filepath}',index_col='Date')
+        # df.set_index(['Date'])
+        print(f'{filename} stock training data loaded.')
+        # print(df.head())
 
-# Upsample minority class
-df_minority_upsampled = resample(df_minority, 
-                                 replace=True,     # sample with replacement
-                                 n_samples= len_1,    # to match majority class
-                                 random_state=42) # reproducible results
-# Combine majority class with upsampled minority class
-df_train = pd.concat([df_majority, df_minority_upsampled])
- 
-# Display new class counts
-print('after resample, the records situation is: \n',df_train.sgn.value_counts())
-df_train_x = df_train.drop(['sgn'], axis=1)
-df_train_y = df_train['sgn']
-print(f'{len(df_train_x)} row training data is prepated.')
+        #*Prapre Training Data
+        # training period: 20110208~20180208
+        df_train = df[df.index < '2018-02-08' ]
+        i += 1
+        #* check training data size 
+        #! skip the stock whose training data is less than 500 rows 
+        if len(df) < 500: 
+            continue
+        # resample the training data 
+        sgn_1 = df_train[df_train['sgn'] == 1]
+        sgn_0 = df_train[df_train['sgn'] == 0]
+        len_1 = len(sgn_1)
+        len_0 = len(sgn_0)
+        print(f'Up record has {len_1} row, while the down records have {len_0} row \n')
 
-#* Prapre Test Data 
-# testing period: 20180208~20190208
-df_test = df[(df.index > '2018-02-08') & (df.index < '2020-02-08')]
-df_test_x = df_test.drop(['sgn'], axis=1)
-df_test_y = df_test['sgn']
-print(f'{len(df_test_x)} row test data is prepared. \n')
+        #resample
+        df_majority = sgn_1
+        df_minority = sgn_0
 
-#* Train Model
-#? runing mutiple algorithm/paramter here, save the model with highest accuracy 
-model_name = os.path.join(f'{cwd}/Model/',f'md_{filename}.joblib')
-clf = RandomForestClassifier(max_depth=2, random_state=42)
-clf_model = clf.fit(df_train_x,df_train_y)
-dump(clf_model, model_name)
-df_test_predict =  clf_model.predict(df_test_x)
-#save prediction results
-df_test['predict'] = df_test_predict  
-df_test.to_csv(f'{cwd}/TPData/tp_{filename}.csv')
+        # Upsample minority class
+        df_minority_upsampled = resample(df_minority, 
+                                        replace=True,     # sample with replacement
+                                        n_samples= len_1,    # to match majority class
+                                        random_state=42) # reproducible results
+        # Combine majority class with upsampled minority class
+        df_train = pd.concat([df_majority, df_minority_upsampled])
+        
+        # Display new class counts
+        print('after resample, the records situation is: \n',df_train.sgn.value_counts())
+        df_train_x = df_train.drop(['sgn'], axis=1)
+        df_train_y = df_train['sgn']
+        print(f'{len(df_train_x)} row training data is prepated.')
+
+        #* Prapre Test Data 
+        # testing period: 20180208~20190208
+        df_test = df[(df.index > '2018-02-08') & (df.index < '2020-02-08')]
+        df_test_x = df_test.drop(['sgn'], axis=1)
+        df_test_y = df_test['sgn']
+        print(f'{len(df_test_x)} row test data is prepared. \n')
+
+        #* Train Model
+        #? runing mutiple algorithm/paramter here, save the model with highest accuracy 
+        model_name = os.path.join(f'{cwd}/Model/',f'md_{filename}.joblib')
+        clf = RandomForestClassifier(max_depth=2, random_state=42)
+        clf_model = clf.fit(df_train_x,df_train_y)
+        dump(clf_model, model_name)
+        df_test_predict =  clf_model.predict(df_test_x)
+        #save prediction results
+        df_test['predict'] = df_test_predict  
+        df_test.to_csv(f'{cwd}/TPData/tp_{filename}.csv')
 
 
-#* checking accuracy rate
-print(f'classification reports \n')
-print(confusion_matrix(df_test_y, df_test_predict))
-print(f'Accuracy rate is {accuracy_score(df_test_y, df_test_predict)}')
+        #* checking accuracy rate
+        print(f'classification reports \n')
+        print(confusion_matrix(df_test_y, df_test_predict))
+        print(f'Accuracy rate is {accuracy_score(df_test_y, df_test_predict)}')
+        print(f'\n stock {filename} has been trained model \n')
+
+    except Exception as ex:
+        print(ex)
+        
+    print(f'Total {i} stocks processed.')
+        
+        
 
 
 
--- ML_Label.py
+++ ML_Label.py
@@ -3,47 +3,65 @@ import pandas as pd
 import os
 from talib import abstract
 
-
-#* Prepare data
-#loading data
-filename = 'A'
+#* Getting stock list 
 # getting currecnt working dirctory
 cwd = os.getcwd()
-df = pd.read_csv(f'{cwd}/OData/{filename}.csv')
+path = cwd + '/OData'
 
-# label records
-# get the previouse day close to label the data
-df['NxtClose'] = df['Close'].shift(-1)
-# calculate the daily percent change
-df['Ptc'] = round(df['NxtClose']/df['Close'] - 1, 4)*100
-# 1 for price going up, 0 for price going down
-df['sgn'] = [1 if  x > 0 else 0 for x in df['Ptc']]
-print(df.head())
+stocklist = []
+for filename in os.listdir(path):
+    if filename.endswith('.csv'):
+        stocklist.append(filename[:-4])
+print(f'There are {len(stocklist)} stocks in S&P500')
 
-# calculate variables
-# calculate ratios as machine learning variables 
-# these ratio calculation will be done by ta-lib
-# moving average 
-ma_list = [5, 10, 20, 30, 60]
-for i in ma_list:
-    df[f'ma{i}'] = pd.Series(abstract.SMA(df, timeperiod = i,price = 'Close'), index = df.index)
-# volumne moving average     
-ma_vol_list = [5, 10, 20, 30, 60]
-for i in ma_vol_list:
-    df[f'ma_vol{i}'] = pd.Series(abstract.SMA(df, timeperiod = i, price = 'Volume'), index = df.index)
-# add previouse 5 days data
 
-col = ['Close','ma5','ma10','ma20','ma30','ma60','ma_vol5','ma_vol10','ma_vol20','ma_vol30','ma_vol60']
-for j in col:
-    for i in range(1,6):
-        df[f'Pre{i}{j}'] = df[f'{j}'].shift(i)
+
+#* Prepare data
+i = 0 
+for filename in stocklist:
+    try:
+        #loading data
+        # filename = 'A'
+        df = pd.read_csv(f'{cwd}/OData/{filename}.csv')
+
+        # label records
+        # get the previouse day close to label the data
+        df['NxtClose'] = df['Close'].shift(-1)
+        # calculate the daily percent change
+        df['Ptc'] = round(df['NxtClose']/df['Close'] - 1, 4)*100
+        # 1 for price going up, 0 for price going down
+        df['sgn'] = [1 if  x > 0 else 0 for x in df['Ptc']]
+        print(df.head())
+
+        # calculate variables
+        # calculate ratios as machine learning variables 
+        # these ratio calculation will be done by ta-lib
+        # moving average 
+        ma_list = [5, 10, 20, 30, 60]
+        for i in ma_list:
+            df[f'ma{i}'] = pd.Series(abstract.SMA(df, timeperiod = i,price = 'Close'), index = df.index)
+        # volumne moving average     
+        ma_vol_list = [5, 10, 20, 30, 60]
+        for i in ma_vol_list:
+            df[f'ma_vol{i}'] = pd.Series(abstract.SMA(df, timeperiod = i, price = 'Volume'), index = df.index)
+        # add previouse 5 days data
+
+        col = ['Close','ma5','ma10','ma20','ma30','ma60','ma_vol5','ma_vol10','ma_vol20','ma_vol30','ma_vol60']
+        for j in col:
+            for i in range(1,6):
+                df[f'Pre{i}{j}'] = df[f'{j}'].shift(i)
+                
+        # clean data
+        # reset index to datatime
+        df = df.set_index(['Date'])
+        # delet Next day close (future value) delet Ptc (future vale)
+        df = df.drop(['NxtClose','Ptc'], axis= 1)
+        # prepare training data
+        df = df.dropna(axis=0)
+        print(df.tail())
+        df.to_csv(f'{cwd}/LIData/li_{filename}.csv')
+        i = i + 1
+        print(f'stock {filename} processed. total {i} stocks processed.')
         
-# clean data
-# reset index to datatime
-df = df.set_index(['Date'])
-# delet Next day close (future value) delet Ptc (future vale)
-df = df.drop(['NxtClose','Ptc'], axis= 1)
-# prepare training data
-df = df.dropna(axis=0)
-print(df.tail())
-df.to_csv(f'{cwd}/LIData/li_{filename}.csv')
+    except Exception as ex: 
+        print(ex)
